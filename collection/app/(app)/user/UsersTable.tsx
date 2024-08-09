ed6b5708755ab30a693c7e810058de93af570c03
Kishan Sambhi <kishansambhi@hotmail.co.uk>
2024-08-09 16:58:21 +0100

    Chore: Move to eactivities columns

--- collection/app/(app)/user/UsersTable.tsx
+++ collection/app/(app)/user/UsersTable.tsx
@@ -6,32 +6,49 @@ import React from "react";
 
 type UserRow = {
     id: number;
-    name: string;
+    firstName: string;
+    surname: string;
     email: string;
+    shortcode: string;
+    position: string;
 };
 
 const columnHelper = createColumnHelper<UserRow>();
 const columns = [
-    columnHelper.accessor("name", {
+    columnHelper.accessor("firstName", {
         cell: (info) => info.getValue(),
-        header: "Name",
-        id: "name",
+        header: "First Name",
+        id: "firstName",
         sortingFn: "alphanumeric",
     }),
-    columnHelper.accessor("email", {
+    columnHelper.accessor("surname", {
         cell: (info) => info.getValue(),
-        header: "Email",
-        id: "email",
+        header: "Surname",
+        id: "surname",
+        sortingFn: "alphanumeric",
+    }),
+    columnHelper.accessor("shortcode", {
+        cell: (info) => info.getValue(),
+        header: "Shortcode",
+        id: "shortcode",
+        sortingFn: "alphanumeric",
+    }),
+    columnHelper.accessor("position", {
+        cell: (info) => info.getValue(),
+        header: "Position",
+        id: "position",
         sortingFn: "alphanumeric",
     }),
 ];
 
 const data: UserRow[] = [
-    { id: 1, name: "Alice", email: "alice@docsoc.co.uk" },
     {
-        id: 2,
-        name: "Bob",
-        email: "bob@docsoc.ic.ac.uk",
+        id: 1,
+        firstName: "Kishan",
+        surname: "Sambhi",
+        email: "kss22@ic.ac.uk",
+        shortcode: "kss22",
+        position: "Vice President",
     },
 ];
 
