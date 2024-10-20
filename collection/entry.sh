ab6c766e3fe8743d67c1157720f632266f47303f
Nishant Aanjaney Jalan <cybercoder.nishant@gmail.com>
2024-10-20 15:14:10 +0100

    run entry with dumb-init and fix script

--- collection/entry.sh
+++ collection/entry.sh
@@ -1,7 +1,8 @@
+#!/usr/bin/dumb-init /bin/sh
+
 # This is to be used with the Docker Container only!
 # The script loads the PSQL certificate and starts the server.
 
-#!/usr/bin/dumb-init /bin/sh
 echo $PG_CERT > ./prisma/postgres.crt
 
 node server.js
