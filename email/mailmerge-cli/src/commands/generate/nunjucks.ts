21a454d751b3807f38d37fcd5282b21bf5c7b278
Kishan Sambhi <kishansambhi@hotmail.co.uk>
2024-10-20 23:58:01 +0100

    feat: add a JSON Backend so we can do lists in nunjucks

--- email/mailmerge-cli/src/commands/generate/nunjucks.ts
+++ email/mailmerge-cli/src/commands/generate/nunjucks.ts
@@ -1,7 +1,9 @@
 import {
     CSVBackend,
+    DataSource,
     GenerateOptions,
     generatePreviews,
+    JSONBackend,
     JSONSidecarsBackend,
     NunjucksMarkdownEngine,
     NunjucksMarkdownTemplateOptions,
@@ -18,8 +20,8 @@ import { DEFAULT_DIRS } from "../../util/constant.js";
 
 export default class GenerateNunjucks extends Command {
     static override args = {
-        csvFile: Args.string({
-            description: "Path to the CSV file to use as the data source for the mailmerge",
+        dataFile: Args.string({
+            description: "Path to the CSV/JSON file to use as the data source for the mailmerge",
             required: true,
         }),
         template: Args.string({
@@ -33,11 +35,20 @@ export default class GenerateNunjucks extends Command {
 
     static override examples = ["<%= config.bin %> <%= command.id %>"];
 
+    static MODE_CSV = "csv";
+    static MODE_JSON = "json";
+
     static override flags = {
         // // flag with no value (-f, --force)
         // force: Flags.boolean({ char: "f" }),
         // // flag with a value (-n, --name=VALUE)
         // name: Flags.string({ char: "n", description: "name to print" }),
+        sourceType: Flags.string({
+            char: "s",
+            description: "Type of data source to use (csv/json)",
+            options: [GenerateNunjucks.MODE_CSV, GenerateNunjucks.MODE_JSON],
+            default: "csv",
+        }),
         output: Flags.string({
             char: "o",
             description: "Path to the directory to output the generated email previews to",
@@ -69,6 +80,20 @@ export default class GenerateNunjucks extends Command {
         }),
     };
 
+    private getDataSource(
+        flags: Awaited<ReturnType<typeof this.parse>>["flags"],
+        args: Awaited<ReturnType<typeof this.parse>>["args"],
+    ): DataSource {
+        switch (flags["sourceType"]) {
+            case GenerateNunjucks.MODE_CSV:
+                return new CSVBackend(join(process.cwd(), args["dataFile"]));
+            case GenerateNunjucks.MODE_JSON:
+                return new JSONBackend(join(process.cwd(), args["dataFile"]));
+            default:
+                throw new Error("Invalid source type");
+        }
+    }
+
     public async run(): Promise<void> {
         const { args, flags } = await this.parse(GenerateNunjucks);
         const engineOptions: NunjucksMarkdownTemplateOptions = {
@@ -89,7 +114,7 @@ export default class GenerateNunjucks extends Command {
                 enableBCC: flags.bcc,
                 enableCC: flags.cc,
             },
-            dataSource: new CSVBackend(join(process.cwd(), args.csvFile)),
+            dataSource: this.getDataSource(flags, args),
             storageBackend: new JSONSidecarsBackend(outputRoot, {
                 type: "dynamic",
                 namer: getFileNameSchemeInteractively,
